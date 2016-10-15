d306864f0e3386538c575ee4a38a6e755a79954e
Timothy Lu <timlu@uw.edu>
2016-10-15 14:27:06 -0700

    Initial Commit

---
+++ watchCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  watchCalc
+//
+//  Created by MacBook on 10/15/16.
+//  Copyright © 2016 timlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
